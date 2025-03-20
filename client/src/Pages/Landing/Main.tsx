a5ce75f43fa8fc75682d5d20467e4c0b311613bd
anshumancodes <anshumanprof01@gmail.com>
2025-03-20 22:40:42 +0530

    refactor:add overflow-x-hidden style

--- client/src/Pages/Landing/Main.tsx
+++ client/src/Pages/Landing/Main.tsx
@@ -11,7 +11,7 @@ import AlphaFinanceFooter from './AlphaFinanceFooter';
 
 function Main() {
   return (
-    <div className="min-h-screen bg-black text-white">
+    <div className="min-h-screen bg-black text-white overflow-x-hidden">
       <Navbar />
       <main className="container mx-auto px-4 py-12 md:px-8 lg:px-16">
         <Hero />
